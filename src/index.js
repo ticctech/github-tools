e989cb60aa0e8c49217f75f9ae99dc6ff45310fd
andrewweston <andrew.weston@propellerhead.co.nz>
2022-11-05 21:48:40 +1300

    tidy api docs step

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 
-import { deleteReleases } from "releases";
+import { deleteReleases } from "./releases.js";
+import { readFileSync, accessSync, constants } from 'node:fs';
+import { cwd } from 'node:process'
 
+import apiSpec from './docs.swagger.json' assert { type: "json" };
 
 const main = async () => {
-    await deleteReleases('report', 20);
+    //const apiSpec = require(`${cwd()}/src/docs.swagger.json`)
+    const res = Buffer.from(JSON.stringify(apiSpec)).toString('base64')
+
+    //let apiSpec = ''
+    // try {
+    //     apiSpec = readFileSync(`${cwd()}/src/docs.swagger.json`, 'base64')
+    // } catch (e) {}
+
+    console.log(res)
+    // await deleteReleases('report', 20);
     // createRelease('report', 'v0.5.0-beta.0');
 }
 
