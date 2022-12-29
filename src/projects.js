e131c8d87d3aa6a19f872c116a4fcb1dcf620eee
andrewweston <andrew.weston@propellerhead.co.nz>
2022-12-30 09:13:34 +1300

    remove swagger stuff

--- src/projects.js
+++
@@ -1,9 +0,0 @@
-import { Octokit } from "octokit";
-
-const octokit = new Octokit({ auth: `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` });
-
-export const publish = async (repo, retain = 0) => {
-    // touch each project
-    // commit
-    // push
-}
