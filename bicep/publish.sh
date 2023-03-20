20cf6568b14eeb344cf36f55806a153a53ad1fb2
andrewweston <andrew.weston@propellerhead.co.nz>
2023-03-20 17:04:36 +1300

    update script

--- bicep/publish.sh
+++ bicep/publish.sh
@@ -1,22 +1,24 @@
 
+RESOURCE_GROUP="app-ticc-dev-ae-rg"
+
 az ts create \
   --name ts-container-app \
   --version "1.0" \
-  --resource-group "app-ticc-stg-ae-rg" \
+  --resource-group "$RESOURCE_GROUP" \
   --location "australiaeast" \
   --template-file "./bicep/container-app.bicep"
 
 az ts create \
   --name ts-api-backend \
   --version "1.0" \
-  --resource-group "app-ticc-stg-ae-rg" \
+  --resource-group "$RESOURCE_GROUP" \
   --location "australiaeast" \
   --template-file "./bicep/api-backend.bicep"
 
 az ts create \
   --name ts-custom-api \
   --version "1.0" \
-  --resource-group "app-ticc-stg-ae-rg" \
+  --resource-group "$RESOURCE_GROUP" \
   --location "australiaeast" \
   --template-file "./bicep/custom-api.bicep"
 
